72fb5cac731fd7be95d078067dd96b66b0d40092
Jerahmeel Francisco <s1920327@usls.edu.ph>
2023-03-26 08:47:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/deliciosoapp/Burger_OneActivity.java
@@ -0,0 +1,14 @@
+package com.example.deliciosoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Burger_OneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_burger_one);
+    }
+}
